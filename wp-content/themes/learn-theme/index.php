84e71830148783b26d46bc0df3f909d9c15135d0
2shakib3 <2shakibhasan3@gmail.com>
2020-05-11 05:20:39 +0600

    customize theme

--- wp-content/themes/learn-theme/index.php
+++ wp-content/themes/learn-theme/index.php
@@ -1,15 +1,27 @@
 <!doctype html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Soft learn</title>
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
 </head>
-<body>
+<h2>
     <div class="container">
         <h2>Hello</h2>
     </div>
+    <?php
+    while (have_posts()) : the_post();
+    ?>
+    <?php the_post_thumbnail(); ?>
+    <h2 <?php the_title(); ?> </h2>
+    <?Php the_content(); ?>
+    <?php
+    endwhile;
+    ?>
+
+    <?php wp_footer(); ?>
 </body>
 </html>
